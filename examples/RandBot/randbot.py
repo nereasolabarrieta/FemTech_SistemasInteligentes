d83e709c404c0f5e394a250638d9f2c9451b7fb2
nereasolabarrieta <nerea.solabarrieta@opendeusto.es>
2021-06-04 20:01:45 +0200

    decision

--- examples/RandBot/randbot.py
+++ examples/RandBot/randbot.py
@@ -73,11 +73,8 @@ class RandBot(interface.Bot):
                 return self.attack(energy)
 
         # Mover aleatoriamente
-        lh = (state["lighthouses"])[1]
-        x_l, y_l = lh["position"]
 
         distancias = []
-        faros_cercanos = []
         for l in state["lighthouses"]:
             x_l, y_l = l["position"]
             d_x1 = abs(cx - x_l)
@@ -91,24 +88,7 @@ class RandBot(interface.Bot):
         i1 = copia.index(min1)
         copia.index(min1).replace(1000)
 
-        min2 = np.amin(copia)
-        i2 = copia.index(min2)
-        copia.index(min2).replace(1000)
-
-        min3 = np.amin(copia)
-        i3 = copia.index(min1)
-        copia.index(min3).replace(1000)
-
-        faros_cercanos.append((state["lighthouses"])[i1])
-        faros_cercanos.append((state["lighthouses"])[i2])
-        faros_cercanos.append((state["lighthouses"])[i3])
-
-        faro = faros_cercanos[0]
-        for f in faros_cercanos:
-            if f["energy"] < faro["energy"]:
-                faro = f
-
-        x_l, y_l = faro["position"]
+        x_l, y_l = (state["lighthouses"])[i1]
 
         if cy > y_l and cx > x_l:
             move = (-1, -1)
